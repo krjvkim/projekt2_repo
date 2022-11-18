ecfdaec45c0279cf763472ac972a20fe4f04279e
krjvkim <104504220+krjvkim@users.noreply.github.com>
2022-11-18 10:58:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve('webappstartseite.html'));
+  res.send('Hello World!')
 })
 
 
