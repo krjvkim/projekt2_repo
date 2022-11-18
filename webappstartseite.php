450f17c16dcd344dec98da969dffbff973407457
krjvkim <104504220+krjvkim@users.noreply.github.com>
2022-11-18 11:13:12 +0100

    Update webappstartseite.php

--- webappstartseite.php
+++ webappstartseite.php
@@ -1,4 +1,20 @@
 <!DOCTYPE html>
+
+<script>
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+</script>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
